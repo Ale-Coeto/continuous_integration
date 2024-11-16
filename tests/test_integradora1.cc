71364aa9574b03da9ad361e059e3d5c3f30d683d
Alejandra Coeto <a01285221@tec.mx>
2024-11-15 21:20:54 -0600

    Test lcs

--- tests/test_integradora1.cc
+++ tests/test_integradora1.cc
@@ -1,9 +1,15 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include "LCS.h"
+#include "Lcs.h"
 
-TEST_CASE("lcs") {
+TEST_CASE("lcs") 
+{
     REQUIRE(algorithms::longestCommonSubstring("abc", "abc") == {1,3});
     REQUIRE(algorithms::longestCommonSubstring("startingpoint", "start") == {1,5});
     REQUIRE(algorithms::longestCommonSubstring("abcabc", "bcabc") == {2,6}); 
+}
+
+TEST_CASE("kmp")
+{
+    REQUIRE(algorithms::kmp());
 }
